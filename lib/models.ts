464363a32ecb9fd9dc2b7410bec621b784ca2e52
Kenneth Sanders <kennysandersschool@gmail.com>
2022-11-27 10:20:04 -0500

    Add subtitle support

--- lib/models.ts
+++ lib/models.ts
@@ -3,7 +3,16 @@ export interface StreamSegmentMetadata {
   readonly duration: number;
 }
 
+export interface Mime {
+  mimeType: string;
+  codecs: string[]; // RFC 6381
+}
+
 export interface StreamMetadata {
   readonly segments: StreamSegmentMetadata[];
+  readonly subtitles?: string;
+  readonly mime: Mime;
   readonly complete: boolean;
+  readonly processedSecs: number;
+  readonly totalDurationSecs: number;
 }
