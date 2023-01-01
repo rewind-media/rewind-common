da36b40166fca1d444a29bd18b03110ae11ea688
Kenneth Sanders <kennysandersschool@gmail.com>
2023-01-01 09:52:50 -0500

    Add logging to readFile util

--- lib/util/file.ts
+++ lib/util/file.ts
@@ -1,6 +1,9 @@
 import { isLocalPath, FileLocation } from "@rewind-media/rewind-protocol";
 import { createReadStream } from "fs";
 import { Readable } from "stream";
+import { RootLogger } from "./log.js";
+
+const log = RootLogger.getChildCategory("CommonUtil");
 
 export function getFile(location: FileLocation): Promise<Readable> {
   if (isLocalPath(location)) {
@@ -14,10 +17,14 @@ export const readFile = async (readable: Readable) =>
   new Promise<Buffer>((resolve, reject) => {
     try {
       const buffs: Uint8Array[] = [];
-      readable.on("data", (it: Uint8Array) => buffs.push(it));
-      readable.on("close", () => {
-        resolve(Buffer.concat(buffs));
-      });
+      readable
+        .on("data", (it: Uint8Array) => buffs.push(it))
+        .on("close", () => {
+          resolve(Buffer.concat(buffs));
+        })
+        .on("error", (e) => {
+          log.error("Failed to read file", e);
+        });
     } catch (e) {
       reject(e);
     }
